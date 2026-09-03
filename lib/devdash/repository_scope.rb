b5bb3f71d4fbd7b7429238b59f1903eaf3a5ddd2
John Rose <john.rose@wealthbox.com>
2026-09-03 15:14:31 -0400

    Enforce repository scope immutability

--- lib/devdash/repository_scope.rb
+++ lib/devdash/repository_scope.rb
@@ -5,4 +5,17 @@ require "json"
 
 module Devdash
   RepositoryScope = Data.define(:key, :repository_names, :label, :configuration_hash)
+
+  class << RepositoryScope
+    alias_method :build, :new
+
+    def new(key:, repository_names:, label:, configuration_hash:)
+      build(
+        key:,
+        repository_names: repository_names.dup.freeze,
+        label:,
+        configuration_hash:
+      )
+    end
+  end
 end
--- spec/devdash/repository_scope_spec.rb
+++ spec/devdash/repository_scope_spec.rb
@@ -14,4 +14,20 @@ RSpec.describe Devdash::RepositoryScope do
     expect { scope.key = "other" }.to raise_error(NoMethodError)
     expect { scope.repository_names << "starburstlabs/repo1" }.to raise_error(FrozenError)
   end
+
+  it "defensively freezes repository names supplied to the constructor" do
+    repository_names = ["starburstlabs/crm-web"]
+    constructed_scope = described_class.new(
+      key: "crm-web",
+      repository_names:,
+      label: "crm-web",
+      configuration_hash: "abc"
+    )
+
+    expect(constructed_scope.repository_names).to be_frozen
+    expect { repository_names << "starburstlabs/repo1" }.not_to raise_error
+    expect(repository_names).to eq(["starburstlabs/crm-web", "starburstlabs/repo1"])
+    expect(constructed_scope.repository_names).to eq(["starburstlabs/crm-web"])
+    expect { constructed_scope.repository_names << "starburstlabs/repo1" }.to raise_error(FrozenError)
+  end
 end
